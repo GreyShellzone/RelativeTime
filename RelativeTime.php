3c1eef6240e3eb067e451d329ba23844d4074683
GreyShellzone <40860628+GreyShellzone@users.noreply.github.com>
2018-07-10 07:22:27 -0700

    Add files via upload

---
+++ RelativeTime.php
@@ -0,0 +1,44 @@
+<?php
+class RelativeTime{
+        
+	public static function GetRelativeTime($dateTime){
+
+		$result = "";
+		$CurrentDate = date('Y-m-d H:i:s');
+		$CurrentDateToSeconds = (int) strtotime(date($CurrentDate));
+		$timeSpan = $CurrentDateToSeconds - ((int) strtotime(date($dateTime)));
+
+		if ($timeSpan <= 60){
+			$result = "about " . $timeSpan . " seconds ago";
+		}
+		else if ($timeSpan <= 3600){
+			
+			$result = $timeSpan > 119 ?
+				"about " . (int) ($timeSpan / 60) . " minutes ago" :
+				"about a minute ago";
+		}
+		else if ($timeSpan <= (3600 * 24)){
+			$result = $timeSpan > ((3600 * 24) - 1) ?
+				"about " . (int) ($timeSpan / 3600) . " hours ago" :
+				"about an hour ago";
+		}
+		else if ($timeSpan <= (3600 * 24 * 30)){
+			$result = $timeSpan > ((3600 * 24 * 30) - 1) ?
+				"about " . (int) ($timeSpan / (3600 * 24)) . " days ago" :
+				"yesterday";
+		}
+		else if ($timeSpan <= (3600 * 24 * 30 * 12)){
+			$result = $timeSpan > ((3600 * 24 * 30 * 12) - 1) ?
+				"about " . (int) ($timeSpan / (3600 * 24 * 30)) . " months ago" :
+				"about a month ago";
+		}
+		else{
+			$result = $timeSpan > (3600 * 24 * 30 * 12) ?
+				"about " . (int) ($timeSpan / (3600 * 24 * 30 * 12)) . " years ago" :
+				"about a year ago";
+		}
+
+		return $result;
+		
+	}
+}
